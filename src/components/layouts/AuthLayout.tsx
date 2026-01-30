fa727c5185e2354e24ccbbe56a1c70ffd3d40ab7
Linda Mensah <lindaakmensah@gmail.com>
2026-01-30 16:45:32 +0000

    simulate auth flow

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -1,6 +1,25 @@
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-linear-to-br from-orange-50 via-white to-orange-100 px-4">
+    <div className="relative min-h-screen w-full flex items-center justify-center px-4 overflow-hidden">
+      {/* top-left gradient */}
+      <div
+        className="absolute -top-32 -left-32 w-64 h-64
+          bg-linear-to-br from-[#FBBF24] via-[#F97316] to-[#EF4444]
+          rounded-full
+          opacity-30
+          pointer-events-none
+          blur-3xl"
+      />
+
+      {/* bottom-right gradient */}
+      <div
+        className="absolute -bottom-32 -right-32 w-64 h-64
+          bg-linear-to-br from-[#FBBF24] via-[#F97316] to-[#EF4444]
+          rounded-full
+          opacity-30
+          pointer-events-none
+          blur-3xl"
+      />
       {children}
     </div>
   );
